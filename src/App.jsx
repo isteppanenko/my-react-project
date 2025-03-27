b7f97be97a0347703f340f077955da061baf418c
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2025-03-27 09:58:58 +0100

    conditional_render

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import WelcomeSection from './components/WelcomeSection/WelcomeSection';
 import Footer from './components/Footer/Footer';
 
 const App = () => {
+  const name = "Niko";
+  const message = 'Hello guys, whats up?'
   return (
     <>
-      <Header/>
+      <Header />
+      <h3>Duo</h3>
+      {name ? <h3>{`Hi, ${name}`}</h3> : <h3>Hello, Guest</h3>}
+      <p>Lorem ipsum dolor sit, { message}amet consectetur adipisicing elit.</p>
       <WelcomeSection />
       <Footer />
     </>
